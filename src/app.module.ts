8e5f1ba4bd671b102c3383a34694005e870590b8
Luan Artacho de Albuquerque <luanartacho@MacBook-Pro-de-Luan.local>
2022-12-17 13:53:05 -0300

    First Commit | Bugfix return post

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from '../src/infra/http/http.module';
+
+@Module({
+  imports: [HttpModule, DatabaseModule],
+})
+export class AppModule {}
